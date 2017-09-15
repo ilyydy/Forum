85d1ad008f4f143d608277b49e58bb17393f0450
ilyydy <ilyydy@gmail.com>
2017-09-15 12:43:58 +0800

    forum to Forum

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ rm -f /etc/nginx/sites-enabled/*
 rm -f /etc/nginx/sites-available/*
 
 # 建立一个软连接
-ln -s -f /root/forum/config/forum.conf /etc/supervisor/conf.d/forum.conf
+ln -s -f /root/Forum/config/Forum.conf /etc/supervisor/conf.d/Forum.conf
 # 不在 sites-available 里面放任何东西
-ln -s -f /root/forum/config/forum.nginx /etc/nginx/sites-enabled/forum
+ln -s -f /root/Forum/config/Forum.nginx /etc/nginx/sites-enabled/Forum
 chmod o+rx /root
-chmod -R o+rx /root/forum
+chmod -R o+rx /root/Forum
 # 重启服务器
 service supervisor restart
 service nginx restart
